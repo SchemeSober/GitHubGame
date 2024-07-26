a495cb78a8fe2650df4dbf6021d0310b8ab81f51
wangdg <wangdg@feifanuniv.com>
2024-07-26 15:53:19 +0800

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("This is main branch");
-        System.out.println("This is remote mainBranch changes!");
-        System.out.println("This is remote mainBranch changes!");
+        System.out.println("This is x-wangdg-2406-1Branch changes!");
     }
 }
