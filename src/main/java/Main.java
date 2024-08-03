5c834be9720122a22f3bea30e54aefe24e32a6fe
SchemeSober <68473995+SchemeSober@users.noreply.github.com>
2024-08-03 09:54:32 +0800

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("This is main branch");
-        System.out.println("This is devBranch changes!");
-        System.out.println("This is devBranch changes!");
-        System.out.println("This is devBranch changes!");
+        System.out.println("This is wangdgBranch changes!");
+        System.out.println("This is wangdgBranch changes!");
+        System.out.println("This is wangdgBranch changes!");
     }
 }
