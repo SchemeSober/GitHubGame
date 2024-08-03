7199874dad2cfd9bac7999953558aef273b7fc65
SchemeSober <68473995+SchemeSober@users.noreply.github.com>
2024-08-03 09:48:48 +0800

    updateMain

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("This is main branch");
-        System.out.println("This is x-wangdg-2406-1Branch changes!");
-        System.out.println("This is x-wangdg-2406-1Branch changes!");
-        System.out.println("This is x-wangdg-2406-1Branch changes!");
+        System.out.println("This is devBranch changes!");
+        System.out.println("This is devBranch changes!");
+        System.out.println("This is devBranch changes!");
     }
 }
